839522e1686b0ae0c211bacf387730ee9bdfd016
Oakekywe <oakekywephyohann99@gmail.com>
2023-02-02 20:08:48 +0630

    Finish testing about router

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,25 @@
     <router-link :to="{name:'Jobs'}">Jobs</router-link>
   </nav>
   <router-view/>
+  <button @click="redirect">Redirect</button>
+  <button @click="goForward">Go Forward</button>
+  <button @click="goBack">Go Back</button>
 </template>
+<script>
+export default {
+  methods:{
+    goBack(){
+      this.$router.go(-1);
+    },
+    goForward(){
+      this.$router.go(1);
+    },
+    redirect(){
+      this.$router.push({name:'Home'});
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -23,9 +41,13 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  border-radius: 5px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: crimson;
+  color: white;
+  padding: 10px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
 import Jobs from '../views/job/Jobs.vue'
 import JobDetail from '../views/job/JobDetail.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -26,6 +27,17 @@ const routes = [
     component: JobDetail,
     props: true
   },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  //catch all
+  {
+    path: '/:catchAll(.*)',
+    component: NotFound
+  },
+
 ]
 
 const router = createRouter({
